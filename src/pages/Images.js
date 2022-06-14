6f94bdc009defb469b6b8601c3a8988b2706d2db
ductran <duc828904@gmail.com>
2022-06-14 22:47:56 +0700

    learn RJ

---
+++ src/pages/Images.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from '../styles/center';
+
+const Images = () => {
+  const handleEvenclick =() =>{
+    console.log('>>> click me')
+  }
+  return (
+    <Container>
+      <h1>Images</h1>
+    </Container>
+  );
+};
+
+export default Images;
